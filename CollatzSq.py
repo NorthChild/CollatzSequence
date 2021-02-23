745b8ef8b65a833285d4d8ba803d66f72d852a99
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-02-23 13:40:11 +0100

    Update CollatzSq.py

--- CollatzSq.py
+++ CollatzSq.py
@@ -1,10 +1,10 @@
 # COLLATZ SEQUENCE
 
-
 # lets create the collatz sequence, where if a number(n) is even: n // 2, and if n is odd: 3 * n + 1
 def collatz(number):
     if (number == 1):
         print('Arrived at final number')
+        return number
     elif (number < 0):
         print('Number has to be larger than 0')
     elif (number % 2 == 0):
@@ -16,11 +16,12 @@ def collatz(number):
         print('Collatz iteration = ', newCollatz)
         return newCollatz
 
-    
-# the users chosen number will be stored as a integer in this variable 
+
+# the users chosen number will be stored as a integer in this variable
 userChoice = int(input('User Choice: '));
 
-# create the loop that iterates on the chosen number till n is 1 
+
+# create the loop that iterates on the chosen number till n is 1
 def finalCollatz(userChoice):
 
     newNum = collatz(userChoice)
@@ -29,4 +30,6 @@ def finalCollatz(userChoice):
         newNum = collatz(newNum)
 
 
+
+# call the function
 finalCollatz(userChoice)
