153044f1b149a0a70e7b7a0ed09990fc42b8bfc4
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-02-23 13:21:08 +0100

    Update CollatzSq.py

--- CollatzSq.py
+++ CollatzSq.py
@@ -7,24 +7,22 @@ def collatz(number):
         print('Number has to be larger than 0')
     elif (number % 2 == 0):
         newCollatz = number // 2
-        print('Collatz result = odd: ', newCollatz)
+        print('Collatz result = ', newCollatz)
         return newCollatz
     else:
         newCollatz = 3 * number + 1
-        print('Collatz result = even: ', newCollatz)
+        print('Collatz result = ', newCollatz)
         return newCollatz
 
 
-userChoice = int(input('User Choice'));
+userChoice = int(input('User Choice: '));
 
 def finalCollatz(userChoice):
 
     newNum = collatz(userChoice)
-    print("NewNum variable: ", newNum)
 
     while (newNum > 1):
         newNum = collatz(newNum)
-        collatz(newNum)
 
 
 finalCollatz(userChoice)
